e2e31c858a11ea03cc525bc81e0a7abc90df47e4
TanishaHirpara <150216881+TanishaHirpara@users.noreply.github.com>
2023-12-08 11:12:25 +0530

    Add files via upload

---
+++ looping with space20.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+ int main()
+
+{
+	int i,j,k;
+for(i=1;i<=5;i++)
+	{
+		for(k=i;k>=1;k--)
+			{
+			printf(" ");
+		}
+			for(j=i;j<=5;j++)
+		{
+			if((i+j)%2==0)
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+		}
+		printf("\n");
+	}
+	
+}
